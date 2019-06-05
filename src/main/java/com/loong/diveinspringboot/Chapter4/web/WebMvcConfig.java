d6c24a2b660062bad583a2e4bd28c5a31b15831c
龙一 <wb-xiongtaolong@cai-inc.com>
2019-06-05 21:22:56 +0800

    * 注解配置：@Configuration（Spring范式注解） * 组件激活：@EnableWebMvc（Spring模块装配） * 自定义组件：WebMvcConfigurer（Spring Bean）

---
+++ src/main/java/com/loong/diveinspringboot/Chapter4/web/WebMvcConfig.java
@@ -0,0 +1,58 @@
+package com.loong.diveinspringboot.Chapter4.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author xiongtaolong
+ * @date 2019-06-04 20:47
+ */
+@Configuration //@Configuration用于定义配置类，可替换xml配置文件，被注解的类内部包含有一个或多个被@Bean注解的方法，这些方法将会被AnnotationConfigApplicationContext或AnnotationConfigWebApplicationContext类进行扫描，并用于构建bean定义，初始化Spring容器。
+@EnableWebMvc //启用spring mvc核心组件提供的能力（就是启用了spring mvc）、支持自定义spring mvc配置的能力
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 原XML方式配置，改成Bean配置形式
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println("在Handler之前执行。。");
+                return true;
+            }
+
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+            }
+
+            @Override
+            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+            }
+        });
+    }
+}
