2256ca0bb5a80ab23bde3e3fadc78f127b59a59b
xiongtaolong <798252779@qq.com>
2021-11-23 19:24:17 +0800

    享元模式

---
+++ src/main/java/com/loong/diveinspringboot/test9/flyweight/ChessPiece.java
@@ -0,0 +1,9 @@
+package com.loong.diveinspringboot.test9.flyweight;
+
+public class ChessPiece {
+
+    private ChessPieceUnit chessPieceUnit;
+    private int positionX;
+    private int positionY;
+
+}
