fbfa2e156e0f5bda0e0f27f2e64c59a84241b838
xiongtaolong <798252779@qq.com>
2021-12-13 18:55:49 +0800

    命令模式

---
+++ src/main/java/com/loong/diveinspringboot/test9/command/Command.java
@@ -0,0 +1,7 @@
+package com.loong.diveinspringboot.test9.command;
+
+public interface Command {
+
+    void execute();
+
+}
---
+++ src/main/java/com/loong/diveinspringboot/test9/command/GotDiamondCommand.java
@@ -0,0 +1,13 @@
+package com.loong.diveinspringboot.test9.command;
+
+public class GotDiamondCommand implements Command{
+
+    public GotDiamondCommand(
+            /*数据*/) { //...
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
